3c7cd8cc55cbd61899cee3412cebbc1337ba5fdf
anagashtarova <ana.gashtarova@students.finki.ukim.mk>
2020-04-03 23:00:32 +0200

    klasata dodadena

---
+++ Faculty.java
@@ -0,0 +1,38 @@
+class Faculty {
+	List<Student> students=new ArrayList<Student>();
+	String facultyName;
+
+	//TODO constructor
+
+	public void addStudent(Student s) {
+		students.add(s);
+	}
+	
+	public void removeStudent(int index) {
+		students.remove(index);
+	}
+	
+
+	public List<Student> getGrades() {
+		return students;
+	}
+
+	public void setGrades(List<Student> students) {
+		this.students = students;
+	}
+
+	public String getFacultyName() {
+		return facultyName;
+	}
+
+	public void setFacultyName(String facultyName) {
+		this.facultyName = facultyName;
+	}
+
+	public Faculty(List<Student> students, String facultyName) {
+		super();
+		this.students = students;
+		this.facultyName = facultyName;
+	}
+
+}
